fa8939dab050e50d906758781bd9a8cf5d344108
Joshua DAVEY <932400863@bun2-206-03.acg.ac.nz>
2021-02-23 12:12:33 +1300

    msg

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+// All custom JS
+alert('hello');
+// Set a click event on hte mobile menu icon
+// Once cicked demostrate jquery chaining
+// Finally demonstrate jquery callback functions
+$('document').ready(function(){
+
+// Select the mobile menu, add a click event, trigger an alert
+$('.wi-day-sunny').click(function(){
+  alert("The paragraph was clicked.");
+});
+
+});
+
+// Document ready ends
